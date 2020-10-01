7f1308b05d562c117c2859bf88fe9c4a25c71eac
aurelien dhellemme <dhellemme.a@gmail.com>
2020-10-01 04:10:36 +0200

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  get "/search", to: "pages#search"
-  get "/home", to: "pages#home"
+  get "search", to: "pages#search"
+  get "home", to: "pages#home"
 
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
