91a0331be367e9ed620d4a333c58df32dc8f28fc
aurelien dhellemme <dhellemme.a@gmail.com>
2020-10-01 04:06:33 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
 
   get "/search", to: "pages#search"
   get "/home", to: "pages#home"
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
   
 end
